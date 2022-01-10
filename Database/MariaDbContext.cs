805020d6ae212320c0f7d2a9bc475b0a935362ae
JurgisKisunas <k.jurgis1997@gmail.com>
2022-01-11 00:19:04 +0200

    MariaDB context changed to in class configuration

--- Database/MariaDbContext.cs
+++ Database/MariaDbContext.cs
@@ -1,16 +1,23 @@
 using GoPassDummy.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
-namespace GoPassDummy
+namespace GoPassDummy.Database
 {
-    public partial class MariaDbContext : Microsoft.EntityFrameworkCore.DbContext
+    public partial class MariaDbContext : Microsoft.EntityFrameworkCore.DbContext, IMariaDbContext
     {
         public virtual DbSet<User> Users { get; set; }
-        
-        public MariaDbContext(DbContextOptions<MariaDbContext> options)
-            : base(options)
-        {
 
+        protected readonly IConfiguration Configuration;
+
+        public MariaDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var connectionString = Configuration.GetConnectionString("MariaDbConnectionString");
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
         }
     }
 }
