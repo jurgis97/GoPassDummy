8b7815e19693703165b49b9d4d3bce2b6efeb2c4
JurgisKisunas <k.jurgis1997@gmail.com>
2022-01-11 18:03:51 +0200

    Updated user creation validations in dto

--- Dtos/CreateUserDto.cs
+++ Dtos/CreateUserDto.cs
@@ -5,12 +5,21 @@ namespace GoPassDummy.Dtos
     public record CreateUserDto
     {
         [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
         public string Name { get; init; }
+
         [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
         public string Surname { get; init; }
+
         [Required]
+        [EmailAddress]
         public string Email { get; init; }
+
         [Required]
+        [Phone]
         public string MobilePhone { get; init; }
     }
 }
